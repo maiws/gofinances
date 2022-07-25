5e0bcaed4690e3b291f66b2385c6161a0f24d7de
Maiara S Dias <maiara.sd@live.com>
2022-07-24 23:03:08 -0300

    Criando componente dos card e estilizando

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import { RFValue } from 'react-native-responsive-fontsize';
+import { HightlightCard } from '../../components/HightlightCard';
 import {
   Container,
   Header,
@@ -9,8 +10,11 @@ import {
   Photo,
   User,
   UserGreeting,
-  UserName
+  UserName,
+  Icon,
+  HightLightCards
 } from './styles';
+
 
 export function Dashboard (){
   return(
@@ -24,8 +28,32 @@ export function Dashboard (){
               <UserName>Maiara </UserName>
             </User>
           </UserInfo>
-        </UserWrapper>
+
+          <Icon name="power"/>
+        </UserWrapper>      
+
       </Header>
+
+      <HightLightCards>
+        <HightlightCard 
+          type='up'
+          title='Entradas' 
+          amount='R$ 17.400,00' 
+          lastTransaction='Última entrada dia 13 de abril'
+        />
+        <HightlightCard 
+          type='down'
+          title='Saídas' 
+          amount='R$ 1.259,00' 
+          lastTransaction='Última saída dia 03 de abril'
+        />
+        <HightlightCard 
+          type='total'
+          title='Total' 
+          amount='R$ 16.141,00' 
+          lastTransaction='01 à 16 de abril'
+        />
+      </HightLightCards>
     </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -11,13 +13,17 @@ export const Header = styled.View`
   height: ${RFPercentage(42)}px;
   background: ${({theme}) => theme.colors.primary};
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const UserInfo = styled.View`
@@ -30,16 +36,34 @@ export const Photo = styled.Image`
   height: ${RFValue(48)}px;
   border-radius: 10px;
 `;
+
 export const User = styled.View`
   margin-left: 17px;
 `;
+
 export const UserGreeting = styled.Text`
   color: ${({theme}) => theme.colors.shape};
   font-size: ${RFValue(18)}px;
   font-family: ${({theme}) => theme.fonts.regular};
 `;
+
 export const UserName = styled.Text`
   color: ${({theme}) => theme.colors.shape};
   font-size: ${RFValue(18)}px;
   font-family: ${({theme}) => theme.fonts.bold};
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({theme}) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+`;
+
+export const HightLightCards = styled.ScrollView.attrs({
+  horizontal : true,
+  showsHorizontalScrollIndicator : false,
+  contentContainerStyle : {paddingHorizontal: 24}
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `;
